bb2784bd493c1bff3424886a193791e4971694f9
Matthias <mthaler@gmx.ch>
2017-04-23 13:35:54 +0200

    remove semicolon

--- Server/index.js
+++ Server/index.js
@@ -1,25 +1,25 @@
-const express = require('express'),
-      bodyParser = require('body-parser'),
-      cors = require('cors'),
-      app = express(),
-      router = require('./router'),
-      db = require('./db');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const router = require('./router')
+const db = require('./db')
 
-var port = 3000;
+var port = 3000
 
-db.connect('mongodb://localhost:27017/mydatabase', function(err) {
+db.connect('mongodb://localhost:27017/mydatabase', function (err) {
   if (err) {
     console.log('Unable to connect to Mongo.')
     process.exit(1)
-  } 
+  }
 })
 
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
-router(app);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+router(app)
 
 
-app.listen(port);
-console.log('Your server is running on port ' + port + '.');
+app.listen(port)
+console.log('Your server is running on port ' + port + '.')
--- Server/router.js
+++ Server/router.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const controller = require('./controllers/controller');
-import path from 'path';
+const express = require('express')
+const controller = require('./controllers/controller')
+const path  = require('path')
 
 module.exports = function(app) {
-  const apiRoutes = express.Router();
+  const apiRoutes = express.Router()
 
   //routes will go here
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'))
   })
-  app.use('/api', apiRoutes);
-  apiRoutes.get('/insert', controller.insertData);
+  app.use('/api', apiRoutes)
+  apiRoutes.get('/insert', controller.insertData)
 }
