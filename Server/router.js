ee20ef1be48d11f0b6770d2c43f250b28a6dc101
Matthias <mthaler@gmx.ch>
2017-04-23 12:21:33 +0200

    added

--- Server/router.js
+++ Server/router.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const controller = require('./controllers/controller');
+import path from 'path';
 
 module.exports = function(app) {
   const apiRoutes = express.Router();
 
   //routes will go here
   app.get('/', (req, res) => {
-    res.send('hello')
+    res.sendFile(path.join(__dirname, './index.html'))
   })
   app.use('/api', apiRoutes);
   apiRoutes.get('/insert', controller.insertData);
---
+++ startMongo.bat
@@ -0,0 +1,1 @@
+d:\dev\MongoDB\mongod.exe --dbpath="d:\dev\mongo-data"
